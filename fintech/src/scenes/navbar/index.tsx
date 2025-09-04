5b79af67724475b189c902826ba2edf260210d6a
shamaskaramat <shams4195140@gmail.com>
2025-09-05 00:57:27 +0500

    piplinestest

--- fintech/src/scenes/navbar/index.tsx
+++ fintech/src/scenes/navbar/index.tsx
@@ -18,7 +18,7 @@ const Navbar = (props: Props) => {
       <FlexBetween gap="0.75rem">
         {/* <PixIcon sx={{ fontSize: "28px" }} /> */}
         <Typography variant="h4" fontSize="16px">
-          Fintech
+          Fintechhfgej
         </Typography>
       </FlexBetween>
 
